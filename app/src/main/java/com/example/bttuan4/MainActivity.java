e4d8f653852375f01ece9d461b30d79374b4d45d
DVANQUYEN <daovanquyen26@gmail.com>
2025-09-27 11:15:21 +0700

    last test

--- app/src/main/java/com/example/bttuan4/MainActivity.java
+++ app/src/main/java/com/example/bttuan4/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.bttuan4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,12 +12,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        btn2 = findViewById(R.id.button2);
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.layout2);
+            }
+        });
+
       //  setContentView(R.layout.layout2);
 
 
